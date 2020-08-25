a2f3535731b2e8f1776696497d065f1f16697437
Martin Nyborg Thomsen <martin.thomsen@gmail.com>
2020-08-25 22:33:56 +0200

    e-step and abl offset adjustments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -753,7 +753,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 137 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -999,7 +999,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -45.15, -8.28, -2.4 }
+#define NOZZLE_TO_PROBE_OFFSET { -45.15, -8.28, -3.5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
